7353cba92fc53f394128d167645d6428eaf3f97e
Étienne Després <etienne@pochour.com>
2015-04-07 17:14:30 -0400

    Premier commit.
    Mouvement de caméra. C’est pas mal ça pour l’instant.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	void LateUpdate () {
+		if(Camera.current != null) {
+			Camera.current.transform.Translate(new Vector3(0.0f, 0.0f, 0.001f + Input.GetAxis("Vertical")));
+			Camera.current.transform.position = new Vector3(
+				Camera.current.transform.position.x, 
+				Camera.current.transform.position.y, 
+				Mathf.Clamp(Camera.current.transform.position.z, -8.0f, -0.12f));
+		}
+	}
+
+}
---
+++ Assets/Scripts/MoonController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonController : MonoBehaviour {
+
+	void FixedUpdate () {
+	  transform.Translate(new Vector3(0.0f, -0.02f, 0.0f));
+	}
+}
